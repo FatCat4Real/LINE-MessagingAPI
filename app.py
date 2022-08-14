7e449cf9bf0bde087897bacef102ece372df328d
FatCat4Real <93673067+FatCat4Real@users.noreply.github.com>
2022-08-14 21:32:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,46 @@
-from flask import Flask, request, abort
+import datetime
+import errno
+import json
+import os
+import sys
+import tempfile
+from argparse import ArgumentParser
+
+from flask import Flask, request, abort, send_from_directory
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    LineBotApiError, InvalidSignatureError
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
-)
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    PostbackAction, DatetimePickerAction,
+    CameraAction, CameraRollAction, LocationAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    MemberJoinedEvent, MemberLeftEvent,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    TextComponent, IconComponent, ButtonComponent,
+    SeparatorComponent, QuickReply, QuickReplyButton,
+    ImageSendMessage)
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_proto=1)
 
-id_1='VlkGmtf89SV460/d/Bqchx4TqssECi9bwuQkxINvPIuN+HN0VzxIf7nlwM9wxGF+LotgiPflAWsMIph5UT9/i43YfrmDK4csMvG3trr6nLUvfd+P2EpBNZUxdFWHcfwMHogJh+TiWKbODpW/ojnqqQdB04t89/1O/w1cDnyilFU='
-id_2='869a0bcabfb772d23057a021509aa213'
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = '869a0bcabfb772d23057a021509aa213'
+channel_access_token = 'VlkGmtf89SV460/d/Bqchx4TqssECi9bwuQkxINvPIuN+HN0VzxIf7nlwM9wxGF+LotgiPflAWsMIph5UT9/i43YfrmDK4csMvG3trr6nLUvfd+P2EpBNZUxdFWHcfwMHogJh+TiWKbODpW/ojnqqQdB04t89/1O/w1cDnyilFU='
 
-line_bot_api = LineBotApi(id_1)
-handler = WebhookHandler(id_2)
-
-
-
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -33,8 +54,12 @@ def callback():
     # handle webhook body
     try:
         handler.handle(body, signature)
+    except LineBotApiError as e:
+        print("Got exception from LINE Messaging API: %s\n" % e.message)
+        for m in e.error.details:
+            print("  %s: %s" % (m.property, m.message))
+        print("\n")
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
@@ -69,12 +94,13 @@ def handle_text_message(event):
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text=event.message.text))
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=event.message.text))
-
-
+    
 if __name__ == "__main__":
-    app.run()
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, port=options.port)
